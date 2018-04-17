253a1274a66997118a2a1ab873a08e72168552ef
yousef <36674918+yousef@users.noreply.github.com>
2018-04-18 02:53:17 +0300

    upload data to firebase with data duplication issue fix

--- Project Code/Arduino_Code/Arduino_Code.ino
+++ Project Code/Arduino_Code/Arduino_Code.ino
@@ -8,47 +8,59 @@
 #include <FirebaseObject.h>
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
-
-
-// Set these to run example.
-#define FIREBASE_HOST "seniorproject-cpit499db.firebaseio.com"
-#define FIREBASE_AUTH "Sx2s1pp92HT3TH93f5nt3iX8h8sz6H794K8SaKXO"
-#define WIFI_SSID "The Void"
-#define WIFI_PASSWORD "S7777777s"
 #include <SoftwareSerial.h>
-SoftwareSerial RFID(2, 4); // RX and TX
+SoftwareSerial RFID(2, 4); // to define software serial manually pin 2 for RX and pin 4 for TX
+//----------------------------------------------------------------------------------------------------------------------------
+// Set these to run example.
+#define FIREBASE_HOST "seniorproject-cpit499db.firebaseio.com"  // DATABASE NAME
+#define FIREBASE_AUTH "Sx2s1pp92HT3TH93f5nt3iX8h8sz6H794K8SaKXO"  // FIREBASE KEY (Web API Key)
+#define WIFI_SSID "STC_HAMDAN"   // NETWORK NAME
+#define WIFI_PASSWORD "s500600ss7"    //NETWORK PASSWORD
 
-int i;
-int x = 0;
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+// VARIABLES
+boolean water = true, coakcola = true, milk = true, juice = true, yogurt = true, pepsi = true;
+int inputRead;
+int count = 0;
 String tag = "";
-unsigned char stopBuzzer[6] = {0xa0, 0x04, 0xb0, 0x00, 0x01, 0xaB};
+String path = "/FRIDGE/sLjJxotWl6YNvcSSTdWAkM0MuZS2"; //Tha path where to put our fridge data
+//----------------------------------------------------------------------------------------------------------------------------
 
+// this is were code start
 void setup()
 {
-  RFID.begin(9600);    // start serial to RFID reader
-  Serial.begin(9600);  // start serial to PC
+  RFID.begin(9600);    // start serial to RFID reader to read and write command;
+  Serial.begin(9600);  // start serial to PC (just for checking - not usufel for)
 
-  // connect to wifi.
+  // beggin connecting to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
+
+  // check if wifi is connect
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
   Serial.println();
   Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP()); // print the current ip address
 
+  // connect to the firebase (fridge database)
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  //RFID.write(stopBuzzer,6);
-  Firebase.remove("RFID TAG");
+  Firebase.remove(path); // empty the database in case of some products is out of the fridge
 }
 
+//----------------------------------------------------------------------------------------------------------------------------
 void loop()
 {
+
+  // start scanning the rfid tags
+
   if (RFID.available() > 0)
   {
-    i = RFID.read();
+    inputRead = RFID.read();
 
     // handle error
 
@@ -58,19 +70,47 @@ void loop()
       return;
     }
 
-    Serial.print(i);
-    tag += String(i);
+    Serial.print(inputRead);
+    tag += String(inputRead);
 
 
 
-    x++;
-    if (x > 16) {
+    count++;
+
+    // rfid reader is sending data byte by byte
+    // use the next condition to split the data (each RFID tag data is a 16 byte in total)
+    if (count > 16) {
       Serial.println("");
-      Serial.println(tag);
-      x = 0;
-      Firebase.pushString("RFID TAG", tag);
-      tag = "";
-      delay(1000);
+
+
+      // these conditions prevent the data dublication
+      if (tag == "002260021191911053316591660103255" && water == true) {
+        Firebase.pushString(path , "water");    // add the product to the fridge database
+        water = false;  // prevent the data to be dublicated
+      }
+      else if (tag == "002260021191911053514433620103255" && coakcola == true) {
+        Firebase.pushString(path , "coak-cola");
+        coakcola = false;
+      }
+      else if (tag == "0022600211919110524112841900223255" && milk == true) {
+        Firebase.pushString(path , "milk");
+        milk = false;
+      }
+      else if (tag == "000000003222162369990251255" && juice == true) {
+        Firebase.pushString(path , "juice");
+        juice = false;
+      }
+      else if (tag == "0000000032221623691010249255" && yogurt == true) {
+        Firebase.pushString(path , "yogurt");
+        yogurt = false;
+      }
+      else if (tag == "0000000032221623691000250255" && pepsi == true) {
+        Firebase.pushString(path , "pepsi");
+        pepsi = false;
+      }
+
+      count = 0; // reset counter to read next RFID tag data
+      tag = ""; // reset tag to
     }
 
   }
